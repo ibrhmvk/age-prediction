45be48fd7c619c6b4c29a4d924f7a10358e0406f
IbrahimVK <ib@logrythmic.com>
2023-09-19 12:16:38 +0530

    updated gender and age

--- app.py
+++ app.py
@@ -8,13 +8,23 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
+        # Check if the post request has the file part
         if 'file' not in request.files:
             return 'No file part', 400
         file = request.files['file']
+        # If the user does not select a file, the browser might
+        # submit an empty file without a filename.
+        if file.filename == '':
+            return 'No selected file', 400
         filename = secure_filename(file.filename)
         file.save(filename)
-        result = DeepFace.analyze(img_path=filename, actions=['age', 'gender'])
-        return 'The estimated age is ' + str(result[0]['age']), 200
+        try:
+            result = DeepFace.analyze(img_path=filename, actions=[
+                                      'age', 'gender'], detector_backend='retinaface')
+            print(result)
+            return 'This is a ' + str(result[0]["dominant_gender"]) + ' and the age is ' + str(result[0]["age"]), 200
+        except Exception as e:
+            return str(e), 400
     return render_template('index.html')
 
 
